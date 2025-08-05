75c53e9cb40ccdab4cb361ce8612e38b04adf30b
Dan <danzemly000@gmail.com>
2025-08-06 00:22:53 +0300

    formatting and testing building

--- app.py
+++ app.py
@@ -4,26 +4,39 @@ import threading
 import time
 import os
 import json
+import sys
 
-app = Flask(__name__)
-window = None  
-sptInstallDir = None #let user set this
-modFolders = [] #pass mod object with data 
+
+if getattr(sys, "frozen", False):
+    # Running in a bundle created by PyInstaller
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.abspath(".")
+
+template_folder = os.path.join(base_path, "templates")
+static_folder = os.path.join(base_path, "static")
+
+app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+
+window = None
+sptInstallDir = None  # let user set this
+modFolders = []  # pass mod object with data
 modConfigs = []
+
 
 class Api:
     def select_SPT_mods_directory(self):
         return select_SPT_mods_directory()
-    
+
     def scrape_configs_from_mod_direcory(self):
         return scrape_configs_from_mod_direcory()
-    
+
     def write_config(self, config_path, data):
-        return write_config(config_path, data)    
-    
-    def set_spt_install_dir(self , path):
+        return write_config(config_path, data)
+
+    def set_spt_install_dir(self, path):
         return set_spt_install_dir()
-    
+
     # Getters for Flask API
     def get_spt_install_dir(self):
         return sptInstallDir
@@ -33,48 +46,53 @@ class Api:
 
     def get_mod_configs(self):
         return modConfigs
-    
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html') 
+    return render_template("index.html")
+
 
 def run_flask():
-    app.run(host='127.0.0.1', port=5000, debug=False)
-    
+    app.run(host="127.0.0.1", port=5000, debug=False)
+
+
 def scrape_configs_from_mod_direcory():
     for folder in modFolders:
         possible_paths = [
-            os.path.join(folder , 'config.json'),
-            os.path.join(folder , 'config', 'config.json')    
+            os.path.join(folder, "config.json"),
+            os.path.join(folder, "config", "config.json"),
         ]
         config_path = None
         for path in possible_paths:
             if os.path.isfile(path):
                 config_path = path
                 break
-            
+
         if config_path:
             try:
-                with open(config_path, 'r' ,encoding='utf-8') as file:
+                with open(config_path, "r", encoding="utf-8") as file:
                     config_data = json.load(file)
-                    
-                modConfigs.append({
-                    'mod_name': os.path.basename(folder),
-                    'mod_path': folder,
-                    'config_path': config_path,
-                    'config_data': config_data
-                })
+
+                modConfigs.append(
+                    {
+                        "mod_name": os.path.basename(folder),
+                        "mod_path": folder,
+                        "config_path": config_path,
+                        "config_data": config_data,
+                    }
+                )
                 print(f"Loaded config from {config_path}")
-               
+
             except Exception as e:
                 print(f"Failed to load config from {folder}: {e}")
         else:
-            print(f"No config.json found in {folder}")   
+            print(f"No config.json found in {folder}")
 
-#get mod dirs from spt user/mods directory
+
+# get mod dirs from spt user/mods directory
 def select_SPT_mods_directory():
-    global sptInstallDir, modFolders  
+    global sptInstallDir, modFolders
     result = window.create_file_dialog(webview.FOLDER_DIALOG)
     if result:
         sptInstallDir = result[0]
@@ -83,11 +101,15 @@ def select_SPT_mods_directory():
             full_path = os.path.join(sptInstallDir, folder)
             if os.path.isdir(full_path):
                 modFolders.append(full_path)
-        return {"status": "success", "sptInstallDir": sptInstallDir, "modFolders": modFolders}
+        return {
+            "status": "success",
+            "sptInstallDir": sptInstallDir,
+            "modFolders": modFolders,
+        }
     else:
         print("No directory selected.")
         return {"status": "cancel"}
-    
+
 
 # def read_config(config_path):
 #     try:
@@ -100,28 +122,31 @@ def select_SPT_mods_directory():
 
 def write_config(config_path, data):
     try:
-        with open(config_path, 'w', encoding='utf-8') as file:
+        with open(config_path, "w", encoding="utf-8") as file:
             json.dump(data, file, indent=4)
         print(f"Config written to {config_path}")
     except Exception as e:
         print(f"Error writing config file {config_path}: {e}")
-        
-        
+
+
 def set_spt_install_dir(path):
     global sptInstallDir, modFolders
     sptInstallDir = path
     modFolders.clear()
     if os.path.isdir(path):
         for folder in os.listdir(path):
-            full_path = os.path.join(path , folder)        
+            full_path = os.path.join(path, folder)
             modFolders.append(full_path)
             return {"status": "success", "sptInstallDir": sptInstallDir}
         else:
             return {"status": "error", "error": "Invalid path"}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     api = Api()
     threading.Thread(target=run_flask, daemon=True).start()
-    time.sleep(1)  
-    window = webview.create_window('SPTweak', 'http://127.0.0.1:5000', width=900, height=670 , js_api=api)
+    time.sleep(1)
+    window = webview.create_window(
+        "SPTweak", "http://127.0.0.1:5000", width=900, height=670, js_api=api
+    )
     webview.start()
